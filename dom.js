3ac871ed8114772e8f4e0cc573b1a364db65d67b
DanielGreene101 <d.b.greene101@gmail.com>
2017-08-16 17:28:15 -0500

    finished

--- dom.js
+++ dom.js
@@ -7,47 +7,53 @@ let email = document.getElementById('email');
 //BUTTONS//
 let cnclBtn = document.getElementById('cancel');
 let donBtn = document.getElementById('donate');
-let perLap = document.getElementById('option1');
-let once = document.getElementById('option2')
 //TABLE//
 let table = document.getElementById('tables');
 
 
+var info = "";
 //ADD NEW DONORS TO DOM//
 function printDonors() {
     let newdonor = donorList.getDonors();
     console.log(donorList.getDonors());
-    var info = "";
     for(let i = 0; i < newdonor.length; i++){
         info += `<p>${newdonor[i].name} ${newdonor[i].email} ${newdonor[i].amount}</p>`;
-        if (perLap === true){
-            info += `<p>PerLap</p>`
-        }else{
-            info += `<p>Single Payment</p>`
-        };
+    newTable();
     };
-    table.innerHTML = info;
 };
 printDonors();
 
+function newTable (){
+    table.innerHTML = info;
+    console.log();
+
+}
+
+
+
 //HANDLE DONATE BUTTON//
 donBtn.addEventListener('click', function (){
-    printDonors();
+    let donor = {};
+    let newPer = ""
     donorList.addDonors(
-        name.value,
-        amount.value,
-        perLap.checked,
-        email.value
+        donor.name = name.value,
+        donor.amount =  amount.value,
+        donor.email = email.value
     );
+//    console.log(donor);
+    newPer += `<p>${donor.name} ${donor.email} ${donor.amount}</p>`;
+    
+        console.log($('#option1'));
+    if ($('#option1').hasClass('active')){
+            newPer += 'PerLap';
+        }else if($('#option2').hasClass('active')){
+            newPer += 'One Time Payment';
+        }else{
+            alert('SELECT PAYMENT TYPE');
+        };
+    table.innerHTML += newPer;
+});
 
-});
-//PERLAP AND ONE TIME PAYMENT BUTTONS//
-perLap.addEventListener('click', function(){
-    perLap === true;
-});
-once.addEventListener('click', function(){
-    perLap === false;
-});
 
 
 //CANCEL BUTTON//
